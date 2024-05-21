88144346d5a2ad89f968b552e3f923f3914c3871
Jiyoongrace <giyun3632@naver.com>
2024-05-21 13:22:49 +0900

    :art: fix: servlet 수정

--- helloweb/src/main/java/helloweb/HelloServlet.java
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -12,6 +12,7 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
         PrintWriter pw = resp.getWriter();
         pw.println("<h1>안녕~</h1>");
     }
