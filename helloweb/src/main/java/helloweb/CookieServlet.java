abcac322bf05261946d665d986f8d030cfcb949f
Jiyoongrace <giyun3632@naver.com>
2024-05-28 16:20:40 +0900

    :art: refactor: WebServlet annotation

--- helloweb/src/main/java/helloweb/CookieServlet.java
+++ helloweb/src/main/java/helloweb/CookieServlet.java
@@ -1,13 +1,17 @@
 package helloweb;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Serial;
 
+@WebServlet("/cookie")
 public class CookieServlet extends HttpServlet {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Override
